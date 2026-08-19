88f12415ba711c3dc41e2a139ccc08bdfbaedd29
Tarique Zaib <Tarique.Zaib@gmail.com>
2026-08-19 11:48:51 +0530

    Enable Swagger in production

--- SkillSwap.API/Program.cs
+++ SkillSwap.API/Program.cs
@@ -111,11 +111,14 @@ if (!string.IsNullOrWhiteSpace(port))
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
